6706cef5bc60e5a40dd30e06e538c0092c0aa153
Cristian Lladó <cllado@xmltravelgate.com>
2019-01-02 16:54:17 +0100

    added slack and changed some styles from home

--- src/app/platform/components/header/header.component.ts
+++ src/app/platform/components/header/header.component.ts
@@ -12,7 +12,6 @@ export class HeaderComponent implements OnInit, OnDestroy {
   subscriptions$: Subscription[];
   profile: any;
   environment: any;
-  show: true;
 
   constructor(public authService: AuthService) {}
 
